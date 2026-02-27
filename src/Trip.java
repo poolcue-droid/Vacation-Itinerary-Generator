10ec5390b40f5737286e7c77cea94c67c5a777ff
LiyaMK09 <liyakoshy06@gmail.com>
2026-02-27 18:05:52 +0530

    Add Trip class to manage travel itineraries

---
+++ src/Trip.java
@@ -0,0 +1,40 @@
+import java.util.List;
+
+public class Trip {
+
+    User user;
+    List<Hotel> hotels;
+    List<Activity> activities;
+
+    public Trip(User user, List<Hotel> hotels, List<Activity> activities) {
+        this.user = user;
+        this.hotels = hotels;
+        this.activities = activities;
+    }
+
+    public void displayTripDetails() {
+
+        System.out.println("\n===== FINAL ITINERARY =====");
+
+        System.out.println("Name: " + user.name);
+        System.out.println("Destination: " + user.destination);
+        System.out.println("Budget: ₹" + user.budget);
+        System.out.println("Days: " + user.days);
+        System.out.println("Travelers: " + user.travelers);
+
+        System.out.println("\n--- Recommended Hotels ---");
+        for (Hotel h : hotels) {
+            System.out.println(h.name + " - ₹" + h.pricePerNight);
+        }
+
+        System.out.println("\n--- Activities ---");
+        for (Activity a : activities) {
+            System.out.println(a.name + " - ₹" + a.cost);
+        }
+
+        System.out.println("\n=== Day-wise Plan ===");
+        for (int i = 1; i <= user.days; i++) {
+            System.out.println("Day " + i + ": Explore " + user.destination + " + Activity");
+        }
+    }
+}
